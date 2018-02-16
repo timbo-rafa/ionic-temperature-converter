6ecfd68702e9b667c87bd67f613bc2f70141636a
Rafael Timbó <timbo.rafa@gmail.com>
2018-02-16 00:34:53 -0500

    Code cleanup

--- src/directives/temperature-converter/temperature-converter.ts
+++ src/directives/temperature-converter/temperature-converter.ts
@@ -1,4 +1,4 @@
-import { Directive, Input, Output, EventEmitter, SimpleChanges, OnChanges, OnInit } from '@angular/core';
+import { Directive, Input, Output, EventEmitter, SimpleChanges, OnChanges } from '@angular/core';
 import { TemperatureConverterProvider } from '../../providers/temperature-converter/temperature-converter'
 /**
  * Generated class for the TemperatureConverterDirective directive.
@@ -30,29 +30,18 @@ export class TemperatureConverterDirective implements OnChanges {
 
   constructor( temperatureConverter: TemperatureConverterProvider) {
     console.log('Hello TemperatureConverterDirective Directive');
-    temperatureConverter.setDefaultTemperature('Celsius')
-    this.temperatureConverter = temperatureConverter//.factory()
-    //console.log('Available temperatures:', this.temperatureConverter.temperatures())
-  }
-  
-  ngOnInit(): void {
-    //this.toChanged()
+    this.temperatureConverter = temperatureConverter
   }
 
   toChanged() {
-    //this.convertTo = this.temperatureConverter.getConverterTo(this.to)
     this.convertFrom = this.temperatureConverter.temperatures[this.to]
-    console.log('convertTo:', this.convertFrom)
-  }
-
-  fromChanged() {
-    
+    //console.log('convertTo:', this.convertFrom)
   }
 
   ngOnChanges(changes : SimpleChanges){
     //console.log('directive ngOnChanges')
-    console.log('Available temperatures:', this.temperatureConverter.temperatures)
-    console.log(changes)
+    //console.log('Available temperatures:', this.temperatureConverter.temperatures)
+    //console.log(changes)
     console.log('from', this.from, 'to', this.to, this.value)
     
     if (changes.from) {
@@ -64,14 +53,8 @@ export class TemperatureConverterDirective implements OnChanges {
       this.toChanged()
     }
 
-    if (!this.convertFrom) {
-      this.toChanged()
-    }
     this.convert = this.convertFrom[this.from]
-    if (this.convert)
-      this.result = this.convert(+this.value)
-
-    console.log('convertTo', this.convertFrom, 'convert', this.convert)
+    this.result = this.convert(+this.value)
     this.resultEmitter.emit(+this.result)
   }
 
--- src/providers/temperature-converter/temperature-converter.ts
+++ src/providers/temperature-converter/temperature-converter.ts
@@ -30,7 +30,6 @@ export class TemperatureConverterProvider {
       Fahrenheit: this.identity
     }
   }
-  defaultTemperature = 'Celsius'
 
   define(temperature, tableOfConvertersObject, newTemperatureToCelsius, newTemperatureToKelvin, newTemperatureToFahrenheit) {
     this.temperatures[temperature] = tableOfConvertersObject
@@ -39,54 +38,34 @@ export class TemperatureConverterProvider {
     this.temperatures.Fahrenheit[temperature] = newTemperatureToFahrenheit
   }
 
-  setDefaultTemperature(temperature) {
-    this.defaultTemperature = temperature
-    return this.defaultTemperature
-  }
-
-  factory() {
-    var factory = {
-      getConverter: function(temperature) {
-        if (arguments.length === 0) temperature = factory.defaultTemperature
-        if (!(temperature in this.temperatures)) throw new Error('Unknown temperature specified: ' + temperature)
-        return this.temperatures[temperature]
-      },
-      defaultTemperature: this.defaultTemperature,
-      temperatures() {
-        return Object.keys(this.temperatures)
-      }
-    }
-    return factory
-  }
-
   // Converter functions
-  identity(temperature: number) : number {
-    console.log('identity', temperature)
+  private identity(temperature: number) : number {
+    //console.log('identity', temperature)
     return temperature
   }
 
-  celsiusToKelvin(celsius: number) : number {
-    console.log('celsiusToKelvin', celsius)
+  private celsiusToKelvin(celsius: number) : number {
+    //console.log('celsiusToKelvin', celsius)
     return celsius + 273.15
   }
 
-  celsiusToFahrenheit(celsius: number) : number {
+  private celsiusToFahrenheit(celsius: number) : number {
     return celsius * 1.8 + 32
   }
 
-  kelvinToFahrenheit(kelvin: number) : number {
+  private kelvinToFahrenheit(kelvin: number) : number {
     return (9/5 * kelvin) - 459.67
   }
 
-  kelvinToCelsius(kelvin: number) : number {
+  private kelvinToCelsius(kelvin: number) : number {
     return kelvin - 273.15
   }
 
-  fahrenheitToCelsius(fahrenheit: number) : number {
+  private fahrenheitToCelsius(fahrenheit: number) : number {
     return (fahrenheit - 32)/ 1.8
   }
 
-  fahrenheitToKelvin(fahrenheit: number) : number {
+  private fahrenheitToKelvin(fahrenheit: number) : number {
     return (fahrenheit + 459.67) * 5/9
   }
 }
