4dd4e1907935205965522d750063f64340ff0cc3
Rafael Timbó <timbo.rafa@gmail.com>
2018-05-02 02:20:14 -0400

    publish.sh commented keytool gen

--- publish-android.sh
+++ publish-android.sh
@@ -1,7 +1,8 @@
 ionic cordova platform add android
 ionic cordova plugin rm org.apache.cordova.console
 ionic cordova plugin rm cordova-plugin-console
-keytool -genkey -v -keystore ../keystore/temperature-converter-release-key.keystore -alias temperature-converter -keyalg RSA -keysize 2048 -validity 10000
+#keytool -genkey -v -keystore ../keystore/temperature-converter-release-key.keystore \
+#    -alias temperature-converter -keyalg RSA -keysize 2048 -validity 10000
 ionic cordova build android --release
 cd platforms/android/app/build/outputs/apk/release
 jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ../../../../../../../../keystore/temperature-converter-release-key.keystore app-release-unsigned.apk temperature-converter
