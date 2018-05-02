b1a536b59c655343faaceca2f0d79fb8a688c732
Rafael Timbó <timbo.rafa@gmail.com>
2018-05-02 02:23:43 -0400

    set -x

--- publish-android.sh
+++ publish-android.sh
@@ -1,3 +1,4 @@
+set -x
 ionic cordova platform add android
 ionic cordova plugin rm org.apache.cordova.console
 ionic cordova plugin rm cordova-plugin-console
@@ -5,5 +6,6 @@ ionic cordova plugin rm cordova-plugin-console
 #    -alias temperature-converter -keyalg RSA -keysize 2048 -validity 10000
 ionic cordova build android --release
 cd platforms/android/app/build/outputs/apk/release
+rm temperature-converter.apk
 jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ../../../../../../../../keystore/temperature-converter-release-key.keystore app-release-unsigned.apk temperature-converter
 $ANDROID_HOME/build-tools/27.0.3/zipalign -v 4 app-release-unsigned.apk temperature-converter.apk
